a27f2988f503b2d53beb9e043f0daba3cd8b2372
biast12 <tobiaspoulsen@hotmail.com>
2024-03-12 22:41:49 +0100

    Update easteregg.js

--- src/_h5ai/public/js/lib/custom/easteregg.js
+++ src/_h5ai/public/js/lib/custom/easteregg.js
@@ -1,41 +1,42 @@
 const isElementLoaded = async (selector) => {
-  while (document.querySelector(selector) === null) {
-    await new Promise((resolve) => requestAnimationFrame(resolve));
-  }
-  return document.querySelector(selector);
+    while (document.querySelector(selector) === null) {
+        await new Promise((resolve) => requestAnimationFrame(resolve));
+    }
+    return document.querySelector(selector);
 };
 
 const easterText = [
-  "Hello World",
-  "Easter Egg",
-  "Trans Rights are Human Rights",
-  "Welcome User",
+    "Hello World",
+    "Easter Egg",
+    "Trans Rights are Human Rights",
+    "Welcome User",
 ];
 const easterTextLegends = [
-  "Welcome Skinbag",
-  "Mozambique Here",
-  "Black Market Here",
-  "Fence Them In, Fence Them Out",
-  "Time to dig up some gold",
+    "Welcome Skinbag",
+    "Mozambique Here",
+    "Black Market Here",
+    "Fence Them In, Fence Them Out",
+    "Time to dig up some gold",
 ];
-isElementLoaded("#customText").then((selector) => {
-  let customText = document.getElementById("customText");
-  let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-  // if randomNumber is 1 will it show the easter egg text
-  if (randomNumber == 1) {
-    let easterTextItem = easterText[Math.floor(Math.random() * easterText.length)];
-    console.log("Easter number is active");
-    customText.innerHTML = easterTextItem;
-    if (easterTextItem === easterText[2]) {
-      let customImg = document.getElementById("customImg");
-      customImg.src = "/_h5ai/public/images/ui/transFlag.jpg";
+isElementLoaded("#customText").then((selector) => {
+    const customText = document.getElementById("customText");
+    const customImg = document.getElementById("customImg");
+    const randomChance = Math.floor(Math.random() * 100) + 1;
+
+    // Check if the randomChance is 1 to show the easter egg text
+    if (randomChance === 1) {
+        const easterTextItem = easterText[Math.floor(Math.random() * easterText.length)];
+        console.log("Easter number is active");
+        customText.innerHTML = easterTextItem;
+        if (easterTextItem === easterText[2]) {
+            customImg.src = "/_h5ai/public/images/ui/transFlag.jpg";
+        }
+    } else if (randomChance === easterTextLegends.length) {
+        const easterTextItem = easterTextLegends[Math.floor(Math.random() * easterTextLegends.length)];
+        console.log("Easter number is active");
+        customText.innerHTML = easterTextItem;
+    } else {
+        console.log("Random chance is: " + randomChance);
     }
-  } else if (randomNumber == 100) {
-    let easterTextItem = easterTextLegends[Math.floor(Math.random() * easterTextLegends.length)];
-    console.log("Easter number is active");
-    customText.innerHTML = easterTextItem;
-  } else {
-    console.log("Random number is: " + randomNumber);
-  }
 });
